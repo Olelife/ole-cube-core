0054ffe36461d5f8b7bb7b9461bb3faa84887904
carlosreyesolelife <carlos.reyes@olelife.com>
2026-03-10 14:55:49 -0500

    New

---
+++ cube.js
@@ -0,0 +1,21 @@
+const { CubejsServer } = require('@cubejs-backend/server');
+
+module.exports = {
+  apiSecret: process.env.CUBEJS_API_SECRET,
+
+  dbType: 'bigquery',
+
+  driverFactory: () => ({
+    type: 'bigquery',
+    projectId: process.env.CUBEJS_DB_BQ_PROJECT_ID,
+    credentials: JSON.parse(process.env.CUBEJS_DB_BQ_CREDENTIALS)
+  }),
+
+  scheduledRefreshContexts: () => [{
+    securityContext: {}
+  }],
+
+  queryRewrite: (query, { securityContext }) => {
+    return query;
+  }
+};
